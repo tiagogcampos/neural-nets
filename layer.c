c7cd735b6a6eaaf87d2ca20508c216ddab8679ee
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2023-05-07 01:31:20 +0100

    move neuron forward method to neuron and change forward signature

--- layer.c
+++ layer.c
@@ -17,19 +17,10 @@ Layer *create_layer(size_t input_shape, size_t output_shape) {
   return layer;
 }
 
-double forward(Neuron *neuron, double *inputs) {
-  double sum = 0;
-  for(size_t i = 0; i < neuron->weights_size; i++) {
-    sum += inputs[i] * neuron->weights[i];
-  } 
-
-  return sum + neuron->bias; 
-}
-
 void forward_layer(Layer *layer, double *inputs, size_t shape) {
   double *forward_pass = malloc(sizeof(double) * shape);
   for(size_t i = 0; i < layer->input_shape; i++) {
-    forward_pass[i] = forward(layer->neurons[i], inputs, shape);
+    forward_pass[i] = forward(layer->neurons[i], inputs);
   }
 }
 
