42ef3bb7db26881b7e539e4550852b02d89c3dbb
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2023-05-07 01:29:42 +0100

    add forward method to layer

--- layer.c
+++ layer.c
@@ -1,7 +1,6 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "layer.h"
 
 Layer *create_layer(size_t input_shape, size_t output_shape) {
@@ -16,6 +15,22 @@ Layer *create_layer(size_t input_shape, size_t output_shape) {
   }
 
   return layer;
+}
+
+double forward(Neuron *neuron, double *inputs) {
+  double sum = 0;
+  for(size_t i = 0; i < neuron->weights_size; i++) {
+    sum += inputs[i] * neuron->weights[i];
+  } 
+
+  return sum + neuron->bias; 
+}
+
+void forward_layer(Layer *layer, double *inputs, size_t shape) {
+  double *forward_pass = malloc(sizeof(double) * shape);
+  for(size_t i = 0; i < layer->input_shape; i++) {
+    forward_pass[i] = forward(layer->neurons[i], inputs, shape);
+  }
 }
 
 size_t get_input_shape(Layer *layer) { return layer->input_shape; }
