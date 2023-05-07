0b89aa4726fd2abd162be8a641892ea3b9944fb7
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2023-05-07 02:04:56 +0100

    move number_of_layers to sequential struct

--- sequential.c
+++ sequential.c
@@ -2,9 +2,31 @@
 #include "layer.h"
 #include "sequential.h"
 
-Sequential *create_sequential_layers(Layer **layers) {
+Sequential *create_sequential_layers(Layer **layers, size_t number_of_layers) {
   Sequential *seq = malloc(sizeof(Sequential));
   seq->layers = layers;
+  seq->number_of_layers = number_of_layers;
 
   return seq;
+}
+
+
+double** forward_sequential(Sequential *sequential, double *inputs) {
+  double **results = malloc(sizeof(double *) * sequential->number_of_layers);
+  double *forward_result = inputs;
+
+  for(size_t i = 0; i < sequential->number_of_layers; i++) {
+    forward_result = forward_layer(sequential->layers[i], inputs);
+    results[i] = forward_result;
+  }
+
+  return results;
+}
+
+void free_sequential(Sequential *sequential) {
+  for(size_t i = 0; i < sequential->number_of_layers; i++) {
+    remove_layer(sequential->layers[i]);
+  }
+
+  free(sequential);
 }
--- sequential.h
+++ sequential.h
@@ -4,9 +4,14 @@
 
 typedef struct {
   Layer **layers;
+  size_t number_of_layers;
 } Sequential;
 
 
-Sequential *create_sequential_layers(Layer **layers);
+Sequential *create_sequential_layers(Layer **layers, size_t number_of_layers);
+
+double** forward_sequential(Sequential *sequential, double *inputs);
+
+void free_sequential(Sequential *sequential);
 
 #endif
