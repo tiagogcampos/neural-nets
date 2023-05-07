ab50317b4390384ae442c14bf3e7040a4a921949
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2023-05-07 17:00:37 +0100

    add basic scalar activation functions

---
+++ src/activations.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+
+double relu(double input) { return fmax(0.1 * input, input); }
+
+double sigmoid(double input) { return 1 / (1 + exp(-input)); }
+
+double binary_step(double input) { return input < 0 ? 0 : 1; }
+
+double linear(double input) { return input; }
